0ebfee3980eb1eca496c2d97d6fad08e7380a198
Aleks <aleks.zmuda@gmail.com>
2026-01-02 16:29:02 +0100

    Refactor: rename auth to cf_auth, fix settings and add API endpoints

--- cityfeel/cityfeel/urls.py
+++ cityfeel/cityfeel/urls.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
-
-# Importujemy Twój gotowy widok z pliku views.py
 from map.views import location_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Tutaj podpinamy adres API pod Twoj¹ funkcjê
+    # Twoje stare API do lokalizacji
     path('api/locations/<int:id>/', location_detail, name='location_detail'),
+
+    # --- NOWE: Podpinamy ca³e API emocji (punkty, komentarze, zdjêcia) ---
+    path('api/emotions/', include('emotions.urls')),
 ]
 
+# Obs³uga zdjêæ (¿eby siê wyœwietla³y w przegl¹darce)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cityfeel/emotions/views.py
+++ cityfeel/emotions/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import EmotionPoint, Comment, Photo
+from .serializers import EmotionPointSerializer, CommentSerializer, PhotoSerializer
 
-# Create your views here.
+class EmotionPointViewSet(viewsets.ModelViewSet):
+    queryset = EmotionPoint.objects.all()
+    serializer_class = EmotionPointSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows comments to be viewed or created.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # Automatycznie przypisz zalogowanego u¿ytkownika jako autora
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows photos to be viewed or uploaded.
+    """
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
