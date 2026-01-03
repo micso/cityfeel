3dc775a03b7678bc0cf7f7f0f7f44ee0e43c2ea1
Michał Sosnowski <m.sosnowski.py@getprintbox.com>
2026-01-03 11:33:59 +0100

    Profile page

--- cityfeel/auth/urls.py
+++ cityfeel/auth/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import RegisterView
+from .views import RegisterView, UserProfileView, UserProfileEditView
 
 app_name = 'cf_auth'
 
@@ -14,4 +14,8 @@ urlpatterns = [
         template_name='auth/login.html'
     ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # User Profile
+    path('user/<int:user_id>/', UserProfileView.as_view(), name='profile'),
+    path('user/edit/', UserProfileEditView.as_view(), name='profile_edit'),
 ]
