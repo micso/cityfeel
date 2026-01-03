481da517d8fe62ec580943e73d3df3d6bcaee5e6
Michał Sosnowski <m.sosnowski.py@getprintbox.com>
2026-01-03 14:21:09 +0100

    New endpoint: api/emotion-points

--- cityfeel/api/urls.py
+++ cityfeel/api/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-
+    path(
+        'emotion-points/',
+        views.EmotionPointCreateView.as_view(),
+        name='emotion_point_create'
+    ),
 ]
