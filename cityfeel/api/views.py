0dfa26a7df4eded34bb10db54cdbe3869ceea6c6
Michał Sosnowski <m.sosnowski.py@getprintbox.com>
2026-01-05 08:44:50 +0100

    #30 Wyswietlanie emocji na mapie

--- cityfeel/api/views.py
+++ cityfeel/api/views.py
@@ -1,9 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Avg, Count
 
 from emotions.models import EmotionPoint
-from .serializers import EmotionPointSerializer
+from map.models import Location
+from .serializers import EmotionPointSerializer, LocationListSerializer
+from .filters import LocationFilter
 
 
 class EmotionPointViewSet(ModelViewSet):
@@ -12,3 +15,36 @@ class EmotionPointViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['emotional_value']
+
+
+class LocationViewSet(ReadOnlyModelViewSet):
+    """
+    ViewSet dla endpointu /api/locations/ (READ-ONLY).
+
+    Zwraca lokalizacje z agregowaną średnią wartością emocjonalną (avg_emotional_value).
+    Średnia liczy ze WSZYSTKICH emotion_points (zarówno publicznych jak i prywatnych).
+
+    Filtrowanie:
+    - ?name=Gdańsk - filtrowanie po nazwie (icontains)
+    - ?lat=54.35&lon=18.64&radius=1000 - filtrowanie po promieniu (metry)
+    - ?bbox=18.5,54.3,18.7,54.4 - filtrowanie po bounding box
+    """
+    serializer_class = LocationListSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = LocationFilter
+
+    def get_queryset(self):
+        """
+        Zwraca queryset z annotacją avg_emotional_value i emotion_points_count.
+        Średnia liczy ze WSZYSTKICH emotion_points (publicznych i prywatnych).
+        Lokalizacje bez emotion_points mają avg_emotional_value=null i count=0.
+        """
+        return (
+            Location.objects
+            .annotate(
+                avg_emotional_value=Avg('emotion_points__emotional_value'),
+                emotion_points_count=Count('emotion_points')
+            )
+            .order_by('-avg_emotional_value', 'name')
+        )
--- cityfeel/map/views.py
+++ cityfeel/map/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from django.db.models import Avg, Count
+
+from emotions.models import EmotionPoint
+from map.models import Location
 
 
 class EmotionMapView(LoginRequiredMixin, TemplateView):
@@ -12,4 +16,54 @@ class EmotionMapView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         # TODO: Add emotion points data when API is ready
         # context['emotion_points'] = EmotionPoint.objects.filter(privacy_status='public')
+        return context
+
+
+class LocationDetailView(LoginRequiredMixin, DetailView):
+    """Widok szczegółowy lokalizacji z publicznymi emotion points i statystykami."""
+    model = Location
+    template_name = 'map/location_detail.html'
+    context_object_name = 'location'
+    login_url = '/auth/login/'
+
+    def get_queryset(self):
+        return Location.objects.annotate(
+            avg_emotional_value=Avg('emotion_points__emotional_value'),
+            emotion_points_count=Count('emotion_points')
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        location = self.object
+
+        # Publiczne emotion points
+        public_points = (
+            EmotionPoint.objects
+            .filter(location=location, privacy_status='public')
+            .select_related('user')
+            .prefetch_related('comment_set')
+            .order_by('-created_at')
+        )
+
+        # Statystyki rozkładu ocen (1-5)
+        emotion_distribution = (
+            EmotionPoint.objects
+            .filter(location=location)
+            .values('emotional_value')
+            .annotate(count=Count('id'))
+            .order_by('emotional_value')
+        )
+
+        # Czy user już ocenił?
+        user_emotion_point = EmotionPoint.objects.filter(
+            location=location,
+            user=self.request.user
+        ).first()
+
+        context.update({
+            'public_points': public_points,
+            'emotion_distribution': emotion_distribution,
+            'user_emotion_point': user_emotion_point,
+        })
+
         return context
