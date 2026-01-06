2a8fe2c10f43d715477b0dd3d4c1ed7d5c9c05fa
bastek2007pl <bastian.suszek@gmail.com>
2026-01-06 12:56:44 +0100

    Chyba to co miało być ogarnięte, to zostało ogarnięte

--- cityfeel/map/models.py
+++ cityfeel/map/models.py
@@ -1,31 +1,14 @@
-from django.contrib.gis.db import models
-
+from django.db import models
 
 class Location(models.Model):
-    """
-    CityFeel Location model for storing geographic points.
-    Stores location names with geographic coordinates using PostGIS.
-    """
-    name = models.CharField(
-        max_length=200,
-        help_text="Nazwa lokalizacji"
-    )
-
-    coordinates = models.PointField(
-        srid=4326,
-        help_text="Współrzędne geograficzne (długość, szerokość)"
-    )
-
-    class Meta:
-        verbose_name = "Lokalizacja"
-        verbose_name_plural = "Lokalizacje"
-        db_table = "map_location"
-        indexes = [
-            models.Index(fields=['name'], name='location_name_idx'),
-        ]
+    name = models.CharField(max_length=255)
+    # Dodajemy nowe pole address
+    address = models.CharField(max_length=255, blank=True, null=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    # Zakładam, że masz te pola do ocen, bo były w serializerze
+    average_rating = models.FloatField(default=0.0)
+    ratings_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
-
-    def get_coordinates_display(self):
-        return f"Lat: {self.coordinates.y}, Lon: {self.coordinates.x}"
