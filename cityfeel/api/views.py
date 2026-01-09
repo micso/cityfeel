ce7f3ff65b67be8de16c1f031d7baf0f80f9e63d
Dawid <dawidwiecek@o2.pl>
2026-01-09 17:21:12 +0100

    Refactor filtering logic to match requirements

--- cityfeel/api/views.py
+++ cityfeel/api/views.py
@@ -57,18 +57,15 @@ class LocationViewSet(ReadOnlyModelViewSet):
     filterset_class = LocationFilter
 
     def get_queryset(self):
-        queryset = Location.objects.all()
-
-        emotional_values = self.request.query_params.get('emotional_value')
-
-        if emotional_values:
-            values_list = emotional_values.split(',')
-            queryset = queryset.filter(emotion_points__emotional_value__in=values_list).distinct()
-
-        return queryset.annotate(
-            avg_emotional_value=Avg('emotion_points__emotional_value'),
-            emotion_points_count=Count('emotion_points')
-        ).order_by('-avg_emotional_value', 'name')
+        return (
+            Location.objects.all()
+            .distinct()
+            .annotate(
+                avg_emotional_value=Avg('emotion_points__emotional_value'),
+                emotion_points_count=Count('emotion_points')
+            )
+            .order_by('-avg_emotional_value', 'name')
+        )
 
 
 class FriendshipViewSet(mixins.CreateModelMixin,
